481168307bd7e1afea99238b8533926f7f602116
SoundSlab <administrator@Mac-mini.local>
2020-08-05 11:50:08 -0400

    revision 1

--- src/Customize/Features.js
+++ src/Customize/Features.js
@@ -1,62 +1,19 @@
 import Selection from './Selection.js'
 import React from 'react'
 import slugify from 'slugify';
-
-
-const FEATURES = {
-    Processor: [
-      {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
-      },
-      {
-        name: 'Professor X AMD Fire Breather with sidewinder technology',
-        cost: 1200
-      }
-    ],
-    "Operating System": [
-      {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
-      },
-      {
-        name: 'Bodhi Linux',
-        cost: 300
-      }
-    ],
-    "Video Card": [
-      {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
-      },
-      {
-        name: 'Mind mild breeze 2000',
-        cost: 1345
-      }
-    ],
-    Display: [
-      {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      },
-      {
-        name: '15.3" HGTV (3840 x 2160) Home makeover edition',
-        cost: 1400
-      },
-    ]
-  };
-
+import {Store} from './Store.js';
 
 
 export default function Features(props) {
     return(
-        Object.keys(FEATURES).map((feature, idx) => {
+        Object.keys(Store).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             
-            const options = FEATURES[feature].map(item => {
+            const options = Store[feature].map(item => {
                 const itemHash = slugify(JSON.stringify(item));
                 return (
-                    <Selection 
+                    <Selection
+                    key={itemHash}
                     itemHash={itemHash}
                     selected={props.selected}
                     feature={feature}
@@ -66,7 +23,6 @@ export default function Features(props) {
                     />
                 );
             });
-
             return (
                 <fieldset className="feature" key={featureHash}>
                 <legend className="feature__name">
